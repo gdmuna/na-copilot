c7b7e198e4135f15df120eff30c481f7c4f00acc
Seele.Clover <37256067+seeleclover@users.noreply.github.com>
2024-08-12 02:09:27 +0800

    chore: add nextjs configuration

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+import dotenv from 'dotenv';
+
+// 加载环境变量
+dotenv.config();
+
+const nextConfig = {
+    // 其他 Next.js 配置
+    env: {
+        DATABASE_URL: process.env.DATABASE_URL // 将 DATABASE_URL 环境变量传递给客户端
+    }
+};
+
+export default nextConfig;
