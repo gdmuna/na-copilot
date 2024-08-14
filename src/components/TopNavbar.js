37a97f98d59ce2f17f6b427c59b5c192bb273c0b
Zollin0 <145448984+Zollin0@users.noreply.github.com>
2024-08-15 00:15:03 +0800

    "顶部导航栏"组件

---
+++ src/components/TopNavbar.js
@@ -0,0 +1,22 @@
+// components/TopNavbar.js
+import React from 'react';
+import Link from 'next/link';
+import Icon from '@/components/Icon';
+
+const TopNavbar = ({ title }) => {
+    return (
+        <div className="flex h-[4rem] w-full px-4 justify-between items-center bg-[#FFFFFF]" style={{ boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)' }}>
+            <div className="flex h-full items-center">
+                <Link href="/">
+                    <Icon type="back" extraclass="text-black-800 cursor-pointer text-[#4a4a4a]" size="20px" />
+                </Link>
+            </div>
+            <div className="flex h-full items-center">
+                <b className="text-xl text-[#4a4a4a]">{title}</b>
+            </div>
+            <div className="w-[20px]"></div>
+        </div>
+    );
+};
+
+export default TopNavbar;
